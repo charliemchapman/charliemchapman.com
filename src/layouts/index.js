3730db8b6585a546734750693e9a732c04b90f3f
Charlie Chapman <charliemchapman@gmail.com>
2018-08-29 20:33:45 -0500

    cant stop styling :(

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,8 +3,8 @@ import Link from "gatsby-link";
 import styles from '../styles/layout.module.css';
 
 export default ({ children }) => (
-  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
-      <header className={styles.headerContainer}>
+  <div>
+    <header className={styles.headerContainer}>
       <div className={styles.headerText}>
         <Link to="/">
           <h3>Charlie Chapman</h3>
@@ -16,6 +16,10 @@ export default ({ children }) => (
         <div><Link to="/archive/">Archive</Link></div>
       </div>
     </header>
-    {children()}
+    <main className={styles.main}>
+      <div className={styles.mainContent}>
+        {children()}
+      </div>
+    </main>
   </div>
 );
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -4,20 +4,20 @@ import oceanBeachTheme from "typography-theme-ocean-beach";
 oceanBeachTheme.headerFontFamily = ['Montserrat', 'sans-serif'];
 oceanBeachTheme.overrideStyles = ({ adjustFontSizeTo, rythm}, options, styles) => ({
     a: {
-        color: "#1B998B",
+        color: "#EA7AF4",
         textDecoration: "none",
         'border-bottom': "1px solid transparent",
         transition: 'border-bottom .25s'
     },
     'a:hover': {
-        'border-bottom': "1px solid #1B998B"
+        'border-bottom': "1px solid #EA7AF4"
     },
     body: {
-        backgroundColor: '#FFFFFF',
-        color: '#1D201F'
+        backgroundColor: '#FBFEF9',
+        color: '#131B23'
     },
     'h1,h2,h3,h4,h5,h6': {
-        color: '#2D517F'
+        color: '#094D72'
     }
 })
 
