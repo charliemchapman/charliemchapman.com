2f8aed84e0455e75877e8f4a9d18521d84e37f2b
Charlie Chapman <charliemchapman@gmail.com>
2018-09-01 15:39:10 -0500

    tweaked about page text

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ export default ({data}) => (
         <Img resolutions={data.file.childImageSharp.resolutions}/>
       </div>
       <div>
-        <p>Hi, I'm Charlie Chapman.  I'm a software developer by trade and amateur podcaster / video editor / motion graphics designer.</p>
+        <p>Hi, I'm Charlie Chapman.  I'm a software developer by trade and amateur podcaster, video editor, motion graphics designer.</p>
         <p>I'm not particular good at any one thing, but I love the process of trying to get better.  Hopefully this blog will reflect that desire to always learn (and erratic swings in interests 😏)</p>
         <p>I'm also a cohost of the podcasts <a href="https://atownfm.com">A-Town FM</a> and <a href="https://atownmovies.com">A-Town at the Movies</a> with my good friend Dan Litz.</p>
         <p>You can find me on <b>Twitter</b> at <a href="https://twitter.com/chuckyc17">@chuckyc17</a> or on <b>Mastodon</b> <a href="https://mastodon.social/@charliemchapman">here</a>.</p>
